8ea421386d8360c7244e13458dad996b7b638927
MIMIEYES <pierreluohao@gmail.com>
2019-08-24 21:31:12 +0800

    update version 0.12

--- src/main/java/io/nuls/contract/constant/ConfigurerConstant.java
+++ src/main/java/io/nuls/contract/constant/ConfigurerConstant.java
@@ -2,6 +2,6 @@ package io.nuls.contract.constant;
 
 public class ConfigurerConstant {
 
-    public final static String DEFAULTPORT="8081";
+    public final static String DEFAULTPORT="18081";
 
 }
