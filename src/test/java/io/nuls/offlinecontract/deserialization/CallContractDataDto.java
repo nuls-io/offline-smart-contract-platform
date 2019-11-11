4aef9ffff3fad4c98d7a514d7f928ef65ea58a0e
MIMIEYES <pierreluohao@gmail.com>
2019-11-11 11:26:03 +0800

    add test case about deserialization of contract data

---
+++ src/test/java/io/nuls/offlinecontract/deserialization/CallContractDataDto.java
@@ -0,0 +1,120 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2017-2019 nuls.io
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.nuls.offlinecontract.deserialization;
+
+
+import io.nuls.base.basic.AddressTool;
+import io.nuls.contract.model.txdata.ContractData;
+
+import static io.nuls.contract.utils.ContractUtil.bigInteger2String;
+
+
+/**
+ * @author: PierreLuo
+ */
+public class CallContractDataDto {
+    private String sender;
+    private String contractAddress;
+    private String value;
+    private long gasLimit;
+    private long price;
+    private String methodName;
+    private String methodDesc;
+    private String[][] args;
+
+    public CallContractDataDto(ContractData call) {
+        this.sender = AddressTool.getStringAddressByBytes(call.getSender());
+        this.contractAddress = AddressTool.getStringAddressByBytes(call.getContractAddress());
+        this.value = bigInteger2String(call.getValue());
+        this.gasLimit = call.getGasLimit();
+        this.price = call.getPrice();
+        this.methodName = call.getMethodName();
+        this.methodDesc = call.getMethodDesc();
+        this.args = call.getArgs();
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContractAddress() {
+        return contractAddress;
+    }
+
+    public void setContractAddress(String contractAddress) {
+        this.contractAddress = contractAddress;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public long getGasLimit() {
+        return gasLimit;
+    }
+
+    public void setGasLimit(long gasLimit) {
+        this.gasLimit = gasLimit;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public String getMethodDesc() {
+        return methodDesc;
+    }
+
+    public void setMethodDesc(String methodDesc) {
+        this.methodDesc = methodDesc;
+    }
+
+    public String[][] getArgs() {
+        return args;
+    }
+
+    public void setArgs(String[][] args) {
+        this.args = args;
+    }
+}
---
+++ src/test/java/io/nuls/offlinecontract/deserialization/CreateContractDataDto.java
@@ -0,0 +1,110 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2017-2019 nuls.io
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.nuls.offlinecontract.deserialization;
+
+
+import io.nuls.base.basic.AddressTool;
+import io.nuls.contract.model.txdata.ContractData;
+import io.nuls.contract.model.txdata.CreateContractData;
+import io.nuls.core.crypto.HexUtil;
+
+/**
+ * @author: PierreLuo
+ */
+public class CreateContractDataDto {
+    private String sender;
+    private String contractAddress;
+    private String alias;
+    private String hexCode;
+    private long gasLimit;
+    private long price;
+    private String[][] args;
+
+    public CreateContractDataDto(ContractData contractData) {
+        CreateContractData create = (CreateContractData) contractData;
+        this.sender = AddressTool.getStringAddressByBytes(create.getSender());
+        this.contractAddress = AddressTool.getStringAddressByBytes(create.getContractAddress());
+        this.alias = create.getAlias();
+        this.hexCode = HexUtil.encode(create.getCode());
+        this.gasLimit = create.getGasLimit();
+        this.price = create.getPrice();
+        this.args = create.getArgs();
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContractAddress() {
+        return contractAddress;
+    }
+
+    public void setContractAddress(String contractAddress) {
+        this.contractAddress = contractAddress;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getHexCode() {
+        return hexCode;
+    }
+
+    public void setHexCode(String hexCode) {
+        this.hexCode = hexCode;
+    }
+
+    public long getGasLimit() {
+        return gasLimit;
+    }
+
+    public void setGasLimit(long gasLimit) {
+        this.gasLimit = gasLimit;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String[][] getArgs() {
+        return args;
+    }
+
+    public void setArgs(String[][] args) {
+        this.args = args;
+    }
+}
